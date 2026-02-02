ca31a07e8e9c4183c848f4aa8c9f2c4fc35e2ad4
Matt Conroy <matt@provenance.io>
2026-02-02 16:46:29 -0500

    - make build again...

--- Formula/provenanced.rb
+++ Formula/provenanced.rb
@@ -11,30 +11,11 @@ class Provenanced < Formula
   depends_on "go@1.23" => :build
 
   def install
-    ENV["CGO_CFLAGS"] = ""
-    ENV["CGO_LDFLAGS"] = ""
-
-    ldflags = %W[
-      -w -s
-      -X github.com/cosmos/cosmos-sdk/version.Name=Provenance
-      -X github.com/cosmos/cosmos-sdk/version.AppName=provenanced
-      -X github.com/cosmos/cosmos-sdk/version.Commit=unknown
-      -X github.com/cosmos/cosmos-sdk/version.BuildTags=ledger,
-      -X github.com/cometbft/cometbft/version.TMCoreSemVer=v0.38.21
-      -X github.com/cosmos/cosmos-sdk/version.Version=nobranch-unknown
-    ].join(" ")
-
-    system "go", "build",
-      "-mod=readonly",
-      "-tags", "ledger",
-      "-trimpath",
-      "-ldflags", ldflags,
-      "-o", "provenanced",
-      "./cmd/provenanced"
+    system "make", "build"
 
     bin.install "build/provenanced"
   end
-
+  
   test do
     assert_match "provenanced version",
       shell_output("#{bin}/provenanced version 2>&1")
