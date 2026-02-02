ce6ed4ef952826c28cd0545bdf663bfd67dd925c
Matt Conroy <matt@provenance.io>
2026-02-02 12:52:14 -0500

    - Add provd formula

---
+++ Formula/provenanced.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Provenanced < Formula
+  desc "Provenance Blockchain CLI (provenanced)"
+  homepage "https://github.com/provenance-io/provenance"
+  url "https://github.com/provenance-io/provenance/archive/refs/tags/v1.27.2.tar.gz"
+  sha256 "2a33f75e91911f836e1b5f8b3730979578b83133342668abc1d689c51d2e53d5"
+  license "Apache-2.0"
+  head "https://github.com/provenance-io/provenance.git", branch: "main"
+
+  depends_on "go@1.23" => :build
+
+  def install
+    ENV["VERSION"] = "v#{version}" unless build.head?
+    ENV["WITH_LEDGER"] = "false"
+    system "make", "build"
+
+    bin.install "build/provenanced"
+  end
+
+  test do
+    assert_match "provenanced version",
+      shell_output("#{bin}/provenanced version 2>&1")
+  end
+end
